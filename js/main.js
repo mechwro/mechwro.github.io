e33c580eb5c568531c7ec08378d0c4917bf7556e
Andrii <mechanic.vulture.ak@gmail.com>
2017-06-19 18:45:13 +0200

    Adding fonts, fix font issue

--- js/main.js
+++ js/main.js
@@ -5,5 +5,19 @@ $(function(){
 	});
 
 
+document.addEventListener('scroll', function(e){
+	e = e || window.event;
+	var btn = document.querySelector('.btn_up'),
+		windowHeight = document.documentElement.clientHeight - 600,
+		scrolled = window.scrollY || document.documentElement.scrollTop;
+	if(scrolled > windowHeight){
+		if(btn.classList.contains('btn_none')) btn.classList.remove('btn_none');
+	} else if(scrolled < windowHeight){
+		if(!btn.classList.contains('btn_none')) btn.classList.add('btn_none');
+	}
+});
+
+
+
 
 	
